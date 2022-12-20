e78db760714754d6429ccf3561e095951abbe1b5
DebianArch <63203414+DebianArch64@users.noreply.github.com>
2022-12-19 19:34:44 -0600

    Add files via upload

---
+++ _app/immutable/chunks/1-a3bd1b37.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-84c1abd9.js";export{t as component};
---
+++ _app/immutable/chunks/2-0eb03371.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-36d930c0.js";export{t as component};
